d761eeb7848df86403d954fcc51d8b12a75167d7
Luke Adams <luke4450@gmail.com>
2015-10-21 11:11:47 -0500

    CLI for testing

--- lib/jadex.ex
+++ lib/jadex.ex
@@ -1,2 +1,19 @@
 defmodule JadEx do
 end
+defmodule JadEx.CLI do
+
+	use Application
+
+	def start(_type, _args) do
+		#Task.start &JadEx.CLI.startCLI/0
+		IO.puts "Nothing"
+	end
+
+	def startCLI() do
+		x = File.read! 'test.jade'
+		IO.puts x
+		ExParsec.parse_value(x, many JadEx.Parse.line) |> IO.inspect
+		IO.puts 'Done'
+		#ExParsec.parse_value x, many sequence([letter, newline]) |> IO.inspect
+	end
+end
